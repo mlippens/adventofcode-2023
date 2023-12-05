fce491915d0f4a08423a89aaa74537d91ab8fe43
Michael Lippens <lippens.m@gmail.com>
2023-12-05 15:37:58 +0100

    fix: day 3 part 1

--- solutions/day-3/main.rb
+++ solutions/day-3/main.rb
@@ -37,50 +37,52 @@ puzzle_matrix.each_with_index do |line, line_number|
       parsed_num += char
     elsif is_num_char
       parsed_num += char
-    elsif !is_num_char && parsed_num != ''
-      num_end_index = i - 1
-      # puts "parsed num from #{num_start_index} to #{num_end_index}: #{parsed_num.to_i}"
-      candidate = parsed_num.to_i
-      parsed_num = ''
-      adjacent_symbol = false
-      if num_start_index.positive? && symbol?(line[num_start_index - 1])
-        adjacent_symbol = true
-        puts "adjacent match left #{candidate}"
-      end
-      if num_end_index < line.length - 1 && symbol?(line[num_end_index + 1])
-        adjacent_symbol = true
-        puts "adjacent match right #{candidate}"
-      end
-      if line_number.positive?
-        adjacent_match_top = ((num_start_index - 1)..(num_end_index + 1)).to_a.any? do |i|
-          if i >= 0 && i < puzzle_matrix[line_number - 1].length
-            match = symbol?(puzzle_matrix[line_number - 1][i])
-            match
-          else
-            false
-          end
-        end
-        if adjacent_match_top
-          adjacent_symbol = true
-          puts "adjacent match top #{candidate}"
-        end
-      end
-      if line_number < puzzle_matrix.length - 1
-        adjacent_match_bot = ((num_start_index - 1)..(num_end_index + 1)).to_a.any? do |i|
-          if i >= 0 && i < puzzle_matrix[line_number + 1].length
-            match = symbol?(puzzle_matrix[line_number + 1][i])
-            match
-          else
-            false
-          end
-        end
-        if adjacent_match_bot
-          adjacent_symbol = true
-          puts "adjacent match bot #{candidate}"
-        end
-      end
-      result_sum += candidate if adjacent_symbol
     end
+    next unless (!is_num_char || i == line.length - 1) && parsed_num != ''
+
+    num_end_index = i - 1
+    # puts "parsed num from #{num_start_index} to #{num_end_index}: #{parsed_num.to_i}"
+    candidate = parsed_num.to_i
+    parsed_num = ''
+    adjacent_symbol = false
+    if num_start_index.positive? && symbol?(line[num_start_index - 1])
+      adjacent_symbol = true
+      puts "adjacent match left #{candidate}"
+    end
+    puts "336: i=#{i} end_index=#{num_end_index} #{line.length}" if candidate == 336
+    if num_end_index < line.length && symbol?(line[num_end_index + 1])
+      adjacent_symbol = true
+      puts "adjacent match right #{candidate}"
+    end
+    if line_number.positive?
+      adjacent_match_top = ((num_start_index - 1)..(num_end_index + 1)).to_a.any? do |i|
+        if i >= 0 && i < puzzle_matrix[line_number - 1].length
+          match = symbol?(puzzle_matrix[line_number - 1][i])
+          match
+        else
+          false
+        end
+      end
+      if adjacent_match_top
+        adjacent_symbol = true
+        puts "adjacent match top #{candidate}"
+      end
+    end
+    if line_number < puzzle_matrix.length - 1
+      adjacent_match_bot = ((num_start_index - 1)..(num_end_index + 1)).to_a.any? do |i|
+        if i >= 0 && i < puzzle_matrix[line_number + 1].length
+          match = symbol?(puzzle_matrix[line_number + 1][i])
+          match
+        else
+          false
+        end
+      end
+      if adjacent_match_bot
+        adjacent_symbol = true
+        puts "adjacent match bot #{candidate}"
+      end
+    end
+    result_sum += candidate if adjacent_symbol
   end
 end
 
